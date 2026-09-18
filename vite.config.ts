0afdf2cdadd66f6350a0ecbd1e110d11fd05254a
Iain Galloway <hello@iaingalloway.com>
2026-09-18 19:38:11 +0000

    Remove playwright

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vitest/config";
-import { playwright } from "@vitest/browser-playwright";
 import adapter from "@sveltejs/adapter-static";
 import { sveltekit } from "@sveltejs/kit/vite";
 
@@ -17,30 +16,9 @@ export default defineConfig({
   ],
   test: {
     expect: { requireAssertions: true },
-    projects: [
-      {
-        extends: "./vite.config.ts",
-        test: {
-          name: "client",
-          browser: {
-            enabled: true,
-            provider: playwright(),
-            instances: [{ browser: "chromium", headless: true }]
-          },
-          include: ["src/**/*.svelte.{test,spec}.{js,ts}"],
-          exclude: ["src/lib/server/**"]
-        }
-      },
-
-      {
-        extends: "./vite.config.ts",
-        test: {
-          name: "server",
-          environment: "node",
-          include: ["src/**/*.{test,spec}.{js,ts}"],
-          exclude: ["src/**/*.svelte.{test,spec}.{js,ts}"]
-        }
-      }
-    ]
+    name: "server",
+    environment: "node",
+    include: ["src/**/*.{test,spec}.{js,ts}"],
+    exclude: ["src/**/*.svelte.{test,spec}.{js,ts}", "src/lib/server/**"]
   }
 });
